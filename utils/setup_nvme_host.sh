b878c2b017dc1289e4b4db186bf23d9df12e0162
taoyl <taoyl@marvell.com>
2017-12-07 13:20:14 +0800

    update sanity checks

--- utils/setup_nvme_host.sh
+++ utils/setup_nvme_host.sh
@@ -5,16 +5,41 @@
 
 # Global settings, default for NanoAP
 
+
+#----------------------------------------------------------------------------
+# Function definition
+#----------------------------------------------------------------------------
 # usage
 function usage () {
     echo "Usage: ${0##*/} [-r|--install] [-h|--help]"
     exit 1
 }
 
+function fix_or_exit () {
+    if [ $install -ne 1 ]; then 
+        exit 1
+    fi
+}
+
 function check_pass () {
     echo
     echo "[ok] all checks pass"
+    echo
     exit 0
+}
+
+function install_tests () {
+    cp -r nvme_tests/py_tests /root/nvme
+    rm -r nvme_tests
+    echo "[ok] nvme tests installed"
+}
+
+function clone_nvmecli () {
+    if [ "$(which git)" == "" ]; then
+        sudo apt-get install -y git
+    fi
+    git clone https://github.com/taoyl/nvme_tests.git 
+    echo
 }
 
 
@@ -56,15 +81,53 @@ else
 fi
 
 #----------------------------------------------------------------------------
+# check nvme-cli
+#----------------------------------------------------------------------------
+if [ "$(which nvme)" == "" ]; then
+    echo "** No nvme-cli found"
+    fix_or_exit
+    clone_nvmecli
+    if [ -e "nvme_tests/nvme" ]; then
+        cp nvme_tests/nvme /usr/local/bin/
+        echo "[ok] nvme-cli installed"
+    else
+        echo "** Fail to install nvme-cli"
+        exit 1
+    fi
+else
+    echo "[ok] nvme-cli check pass"
+fi
+
+#----------------------------------------------------------------------------
+# check host tests
+#----------------------------------------------------------------------------
+if [ -e "/root/nvme/run_nvme_test.py" ]; then
+    echo "[ok] nvme tests check pass"
+else
+    echo "** No nvme tests found"
+    fix_or_exit
+    if [ -e "nvme_tests/py_tests" ]; then
+        install_tests
+    else
+        clone_nvmecli
+        if [ -e "nvme_tests/py_tests" ]; then
+            install_tests
+        else
+            echo "** Fail to install nvme tests"
+            exit 1
+        fi
+    fi
+fi
+
+
+#----------------------------------------------------------------------------
 # check python version
 #----------------------------------------------------------------------------
 py_version=`python3 -V`
 py_ver_m=`python3 -V | cut -d' ' -f2 | cut -d'.' -f1`
 if [ "$py_ver_m" != "3" ]; then
     echo "*** No python3 found"
-    if [ $install -ne 1 ]; then
-        exit 1
-    fi
+    fix_or_exit
     sudo apt-get install -y python3
     sudo apt-get install -y python3-pip
     echo
@@ -81,9 +144,7 @@ fi
 nose_installed=`python3 -c "import nose" 2>&1 | grep 'ImportError'`
 if [ "$nose_installed" != "" ]; then
     echo "** No python-nose found"
-    if [ $install -ne 1 ]; then
-        exit 1
-    fi
+    fix_or_exit
     if [ "$(which pip3)" == "" ]; then
         sudo apt install -y python3-pip
     fi
@@ -94,23 +155,6 @@ else
     echo "[ok] python-nose check pass"
 fi
 
-#----------------------------------------------------------------------------
-# check nvme-cli
-#----------------------------------------------------------------------------
-if [ "$(which nvme)" == "" ]; then
-    echo "** No nvme-cli found"
-    if [ $install -ne 1 ]; then 
-        exit 1
-    fi
-    if [ -e "nvme" ]; then
-        cp nvme /usr/local/bin/
-        echo "[ok] nvme-cli installed"
-    else
-        echo "** Fail to install nvme-cli"
-    fi
-else
-    echo "[ok] nvme-cli check pass"
-fi
 
 #----------------------------------------------------------------------------
 # check SSH server
@@ -118,9 +162,7 @@ fi
 ssh_running=`ps -e | grep sshd`
 if [ "$ssh_running" == "" ]; then
     echo "** No SSH server found"
-    if [ $install -ne 1 ]; then
-        exit 1
-    fi
+    fix_or_exit
     sudo apt-get install -y openssh-server
     sudo service ssh start
     echo
@@ -132,34 +174,35 @@ fi
 #----------------------------------------------------------------------------
 # check RSA keys
 #----------------------------------------------------------------------------
-rsa_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRZVWATztzOoa9LyDIB8FeN6TyFkOY5/sNv4a0P5VXWyqWsqwc7xQtFI0uVU/wtaFMKRKPU4tCrFm1Q78Zx13LbNUtmmeNK4dgwsCrszirHUW1D13WKxEzt2yzeiQJmudW1LoCF/wF8QjkG5ecbcxPXlMA5rjNJZYcSq+0teDsHa5/x+KMiO79WIJu91VRlEVucmmxyDYtvsjv1l+tmoZokSD9U0gq+rxGHnBgA+s8/ljS0D3OaQbkXU6SLA0cUt9VB2I0f2AOOUS6OPARmJp6qQwHQ1azv2BcKPKPy/Qe9ahmek5yRApg1pqnxU3JlmTlsI83K4Gy25ahfJNvyqCP"
+rsa_key="AAAAB3NzaC1yc2EAAAABJQAAAQEA4m8n7VsLrGRvG/jUY06S4qvQA+HHMOPVpk5InKtt5CfCl65MZPxoKowqcQSg8psmy5+No+WYaduDNC8dot5SPIzFw4D3xa00/rg0OwkGPNlU+zQF2NmQjDEtf35gf3LtcHZlZ1pqmNQit/sVEZy24hpKNXGqes83/vi72fiXG+oLnHtEgZOBEoULQexfJwh3UFI6nXHCmB9BfKlGxlO8zkFfGIIcw/Yfbk96EWypnlmSRwjhq1Sv1jemq8xIfTK66SNuZ/MG/btd/ciu9MQPEs81E7gLqKRLBkWJ9gDv34hhYhUs99M3dNWgpaSCTESN9LQNz+TvkEly9WiWy2aTMw=="
 key_file="/root/.ssh/authorized_keys"
 if [ ! -e $key_file ]; then
     echo "** No authorized_keys file found in root/.ssh"
-    if [ $install -eq 1 ]; then
-        if [ ! -e "/root/.ssh" ]; then
-            mkdir -p /root/.ssh 
-        fi
-        echo $rsa_key > $key_file
-        echo "[ok] create authorized_keys file with valid rsa key"
-        check_pass
-    else
-        exit 1
+    fix_or_exit
+    if [ ! -e "/root/.ssh" ]; then
+        mkdir -p /root/.ssh 
     fi
+    echo "ssh-rsa $rsa_key" > $key_file
+    echo "[ok] create authorized_keys file with valid rsa key"
+    check_pass
 fi
+# authorized_keys file exists, check if valid rsa key exists
 key_found=0
 # must use process substitution, else cannot modify global variables
 while read -r line; do
-    if [ "$(echo $line | tr -d '\n')" == "$rsa_key" ]; then
+    if [ "$(echo $line | tr -d '\n' | cut -d' ' -f2)" == "$rsa_key" ]; then
         key_found=1
+        break
     fi
 done < <(cat $key_file)
 
 if [ $key_found -eq 1 ]; then
     echo "[ok] ssh rsa key check pass"
 else
-    echo "** No valid rsa key found, will add one"
-    echo $rsa_key >> $key_file
+    echo "** No valid rsa key found"
+    fix_or_exit
+    echo "ssh-rsa $rsa_key" >> $key_file
+    echo "[ok] add a valid rsa key"
 fi
 
 check_pass
