586764fd310f104d2547b244910ce483c1cbbfb7
Nero <nerotao@foxmail.com>
2019-01-10 13:52:35 +0800

    Add inline-comment usage for svndiffwrap.sh

--- scripts/svndiffwrap.sh
+++ scripts/svndiffwrap.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Edit /home/yuliang.tao/.subversion/config, 
+# add the following line to [helpers] section
+# diff-cmd = /home/yuliang.tao/Utils/scripts/svndiffwrap.sh
 
 vimdiff ${6} ${7}
